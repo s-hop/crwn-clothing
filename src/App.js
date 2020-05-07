1ec9cdb9bea011b0452eac573c9a45b00b4a9cee
Steve Hopkins <steve.hopkins@outlook.com>
2020-05-07 13:15:36 +0100

    broken homepage into smaller cmpnts. Menu styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './App.css';
 
-import HomePage from './HomePage.component';
+import HomePage from './pages/HomePage/HomePage.component';
 
 function App() {
   return (
