85d58c5bce0451af7038ba2dcdcd12e0c3bd9c79
Steve Hopkins <steve.hopkins@outlook.com>
2020-05-09 12:30:18 +0100

    redirect to home if user signed in

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,6 +5,10 @@ import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const store = createStore(
+  rootReducer,
+  applyMiddleware(...middlewares) /* preloadedState, */ +
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
