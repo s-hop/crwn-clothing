5f3045aac1a764c18ad075561e4e549a7887c4a2
Steve Hopkins <steve.hopkins@outlook.com>
2020-05-16 11:53:01 +0100

    updated data pull from Firebase to use Promises

--- src/pages/Shop/Shop.component.jsx
+++ src/pages/Shop/Shop.component.jsx
@@ -28,7 +28,13 @@ class ShopPage extends React.Component {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
 
-    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+    // fetch(
+    //   'https://firestore.googleapis.com/v1/projects/crwnclothing-e82bf/databases/(default)/documents/collections'
+    // )
+    //   .then(response => response.json())
+    //   .then(collections => console.log(collections));
+
+    collectionRef.get().then(snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
       this.setState({ loading: false });
